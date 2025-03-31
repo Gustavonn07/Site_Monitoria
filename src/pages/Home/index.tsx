22d67c143a43f3c5498330a5d36850991612c2b5
Gustavonn07 <gustavonepnog@gmail.com>
2025-03-31 17:12:34 -0300

    style: home style page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import { useOutletContext } from "react-router-dom";
-import { mainLayoutContext } from "@/layout";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../../components";
-import toast from "react-hot-toast";
+import './animation.css'
 
 export const Home = () => {
-  const { setHead } = useOutletContext<mainLayoutContext>();
-
-  React.useEffect(() => {
-    setHead({ title: "HOME" });
-  }, [setHead]);
-
   return (
-    <div className="w-full bg-red-200 flex justify-center">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>Hover</TooltipTrigger>
-          <TooltipContent>
-            <p>Add to library</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+    <div className="w-full flex relative overflow-hidden bg-primary-800">
+      <h1 className='absolute top-16 left-1/2 -translate-1/2 z-50 text-4xl font-medium text-gray-300 bg-primary-600 shadow-lg px-5 py-3 rounded-lg'>Selecione sua turma</h1>
 
-      <button onClick={() => toast("Hello World")}>Click</button>
+      <div
+        className="w-1/2 hover:scale-105 duration-150 h-screen bg-cover bg-center flex justify-center items-center relative group"
+        style={{
+          backgroundImage: "url('images/home_code.jpg')",
+          clipPath: "polygon(0 0, 100% 0, 90% 100%, 0% 100%)",
+        }}
+      >
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/60 to-black/60 transition-opacity duration-300 group-hover:opacity-40" />
+        <h2 className='text-white text-4xl font-bold z-20'>Programação 02</h2>
+        <div className="absolute top-0 right-0 w-12 h-full bg-gradient-to-l from-black/60 to-transparent pointer-events-none group-hover:animate-clip" />
+      </div>
+
+      <div
+        className="w-1/2 hover:scale-105 duration-150 h-screen bg-cover bg-center flex justify-center items-center relative group"
+        style={{
+          backgroundImage: "url('images/home_computer.jpg')",
+          clipPath: "polygon(10% 0, 100% 0, 100% 100%, 0 100%)",
+        }}
+        >
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/40 to-black/40 transition-opacity duration-300 group-hover:opacity-0" />
+        <h2 className='text-white text-4xl font-bold z-20'>Autoração Multimídia 02</h2>
+        <div className="absolute top-0 left-0 w-12 h-full bg-gradient-to-r from-black/60 to-transparent pointer-events-none group-hover:animate-clip-reverse" />
+      </div>
     </div>
   );
 };
+
